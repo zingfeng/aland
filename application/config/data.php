f6816df9925e6a30bed65562cdec914b6719326e
Light <quangpkv@gmail.com>
2019-11-06 09:45:50 +0700

    Update data.php

--- application/config/data.php
+++ application/config/data.php
@@ -11,14 +11,33 @@ $config['test_type'] = array(
 	//30 => array('name' => 'Bài tập ghi âm'),
 );
 $config['cambridge_test_type'] = array(
-	1 => array('name' => 'Điền từ vào chỗ trống - Nối từ'),
-	2 => array('name' => 'Điền từ vào chỗ trống - Sắp xếp từ'),
-	3 => array('name' => '...........'),
-	4 => array('name' => '...........'),
-	5 => array('name' => '...........'),
-	6 => array('name' => '...........'),
-	7 => array('name' => '...........'),
-	8 => array('name' => '...........'),
+    //Reading & Writing
+	1 => array('name' => '[Reading & Writing] Xác định tính đúng/sai của dữ kiện với hình ảnh'),
+	2 => array('name' => '[Reading & Writing] Đánh vần từ theo hình ảnh'),
+	3 => array('name' => '[Reading & Writing] Điền từ vào chỗ trống - Chọn từ trong danh sách có sẵn'),
+	4 => array('name' => '[Reading & Writing] Điền từ vào chỗ trống - Không có từ cho trước'),
+	5 => array('name' => '[Reading & Writing] Trả lời câu hỏi ngắn về câu chuyện kể bằng hình ảnh.'),
+	6 => array('name' => '[Reading & Writing] Nối câu đúng để hoàn thành hội thoại'),
+	7 => array('name' => '[Reading & Writing] Trắc nghiệm chọn đáp án đúng'),
+	8 => array('name' => '[Reading & Writing] Viết số từ quy định để hoàn thành câu'),
+	9 => array('name' => '[Reading & Writing] Viết đoạn văn ngắn'),
+
+	//Listening
+	10 => array('name' => '[Listening] Nối tên với người trong tranh'),
+	11 => array('name' => '[Listening] Nghe trả lời câu hỏi về đứa trẻ hoặc động vật'),
+	12 => array('name' => '[Listening] Điền từ để hoàn thành câu'),
+	13 => array('name' => '[Listening] Nghe và chọn hình ảnh đúng'),
+	14 => array('name' => '[Listening] Nghe - Ghép từng người được nói tới với hình ảnh/đối tượng tương ứng'),
+	15 => array('name' => '[Listening] Nghe và tô màu, viết vào tranh theo chỉ dẫn:'),
+
+	//Speaking
+	16 => array('name' => '[Speaking] Hỏi và trả lời về 1 tấm ảnh'),
+	17 => array('name' => '[Speaking] Trả lời các câu hỏi ngắn về tấm ảnh.'),
+	18 => array('name' => '[Speaking] Trả lời các câu hỏi ngắn về đồ vật trong ảnh.'),
+	19 => array('name' => '[Speaking] Tìm điểm khác nhau trong 2 bức ảnh'),
+	20 => array('name' => '[Speaking] Kể chuyện'),
+	21 => array('name' => '[Speaking] Hỏi và trả lời về bản thân'),
+	22 => array('name' => '[Speaking] Yêu cầu và đưa ra thông tin về 2 tình huống tương tự.'),
 );
 $config['course_class_type'] = array(
 	1 => 'Bài học',
